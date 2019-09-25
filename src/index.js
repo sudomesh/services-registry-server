b5241ee35eccdbd88964ff67acf582462fdad1f5
Jackson Callaway <jackson.callaway@coinbase.com>
2019-09-24 22:18:39 -0700

    add a CORS bypass for development

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import Express from 'express';
+import ZoneFileRequestHandler from './RequestHandlers/ZoneFile';
 
 const PORT = 3000;
 const server = Express();
 
-server.get('/', (req, res) => res.send('Hello World!'))
+server.use(Express.static('client-dist'));
+
+server.use(function(req, res, next) {
+    if(process.env.NODE_ENV !== 'production'){
+        res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    }
+    next();
+  });
+
+server.get('/zonefile', ZoneFileRequestHandler)
 
 server.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
